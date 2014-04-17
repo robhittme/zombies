ff9570d468b54392e3e99b6f7ed0224532924f77
Robert Hitt <rphitt1@gmail.com>
2014-04-17 10:20:18 -0400

    prototype upload

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,56 +8,63 @@ function Human(name, weapon) {
 	this.isDead = false;
 	this.health = 100;
 	this.weapon = weapon;
-	this.bitten = function() {
+}
+Human.prototype.bitten = function() {
 		this.isZombie = true;
 		this.killSelf = function() {
 			this.isDead= true;
 
-		}
+		};
 		this.health=100;
 
 	};
-	}	
+
 
 function ZombieShit(type, damage) {
 	this.type= type;
 	this.damage = damage;
-	this.attack = function(target) {
-		target.health = target.health - this.damage;
-		if(target.health <=0) {
-			alert("Oh No! " + target.name + " will soon be a zombie!");
-			target.isZombie = true;
-			target.health = 100;
-		}
-	}
 }
+ZombieShit.prototype.attack = function(target) {
+	target.health = target.health - this.damage;
+	if(target.health <=0) {
+		alert("Oh No! " + target.name + " will soon be a zombie!");
+		target.isZombie = true;
+		target.health = 100;
+		}
+	};
+
 function Weapon(type, damage) {
-	this.type= type || "null"
+	this.type= type || "null";
 	this.damage = damage || 1 ;
-	this.attack = function(target) {
-		target.health = target.health - this.damage;
-		if(target.health <=0) {
-			alert("Oh No! " + target.name + " will soon be dead!");
-			target.isDead = true;
-		}
-	}
 }
+Weapon.prototype.attack = function(target) {
+	target.health = target.health - this.damage;
+	if(target.health <=0) {
+		alert("Oh No! " + target.name + " will soon be dead!");
+		target.isDead = true;
+		}
+	};
+
 function Zombie(name, weapon) {
 	this.isZombie = true;
 	this.isDead = false;
 	this.health = 100;
 	this.weapon = weapon;
-	this.bite = function(human) {
+}
+Zombie.prototype.bite = function(human) {
 		human.bitten();
 	};
 
-}
+
 
 var fist= new ZombieShit("bodily", 10);
+var pistol = new Weapon("gun",20);
 var shotgun= new Weapon("gun",30);
 var bat = new Weapon("club", 15);
 var zom= new Zombie("zom", fist); 
-var robert = new Human("Robert", bat);
+var robert = new Human("Robert", pistol);
 var shawn = new Human("Shawn", shotgun);
 var brooke = new Human("Brooke",bat);
+var john = new Human ("john", bat);
+
 
